db86e7fd73d75b65936c86f4fe5d763bc5a89e29
tuankhoi911 <17520648@gm.uit.edu.vn>
2019-05-27 19:34:30 +0700

    update api

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from "../../services/todoData.service";
+import { Todo } from "../../models/todo";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  providers: [TodoDataService]
 })
 export class HomeComponent implements OnInit {
 
-  pbLogo = 'assets/images/pbLogo.png'; 
+  pbLogo = 'assets/images/pbLogo.png';
 
-  constructor() { }
+  patients: Todo[] = [];
+
+  constructor(private todoDataService: TodoDataService) { }
 
   ngOnInit() {
+    this.todoDataService
+      .getAllTodos()
+      .subscribe(
+        (todos) => {
+          this.patients = todos;
+        }
+      );
   }
-
 }
